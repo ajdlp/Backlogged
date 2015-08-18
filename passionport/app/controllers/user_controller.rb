57c1a31831551c8d36625518efa0d55e72446484
Alex DeLaPena & Michael Wong <pair+ajdlp.mikey2XU@devbootcamp.com>
2015-08-18 11:54:57 -0700

    add user login via session routes and controller

--- passionport/app/controllers/user_controller.rb
+++ passionport/app/controllers/user_controller.rb
@@ -3,5 +3,21 @@ class UserController < ApplicationController
   end
 
   def index
+    @user = User.find(user_params)
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+end
+
+private
+def user_params
+  params.require(:user).permit(:username, :password)
 end
--- passionport/config/routes.rb
+++ passionport/config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'user/index'
+  get 'sessions/new'
+
+  post   'login'   => 'sessions#create'
+
+  delete 'logout'  => 'sessions#destroy'
+
+  root 'sessions#new'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -14,7 +22,6 @@ Rails.application.routes.draw do
     resources :users
   end
 
-  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
