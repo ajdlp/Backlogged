7a9067f8c04a03815537b0462d448042d6d1acff
Alex DeLaPena & Michael Wong <pair+ajdlp.mikey2XU@devbootcamp.com>
2015-08-18 18:07:24 -0700

    Add seed file and games view

--- passionport/app/controllers/games_controller.rb
+++ passionport/app/controllers/games_controller.rb
@@ -1,2 +1,12 @@
 class GamesController < ApplicationController
+  def show
+     @games = current_user.games
+     @owned_games = current_user.owned_games
+     render 'show'
+  end
+
+  def new
+
+  end
+
 end
--- passionport/db/seeds.rb
+++ passionport/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'httparty'
+
+@nes = HTTParty.get("https://www.kimonolabs.com/api/8g4e6ylc?apikey=b25MqRZiGNLAUELhKxzrHnttW4ZsbwPZ")
+
+@nesgames = @nes["results"]["collection1"]
+
+@nesgames.each do |game|
+  Game.create(title: game["property1"]["text"], boxart: game["property2"]["src"], platform: game["property3"]["text"], release_year: game["property4"] )
+end
