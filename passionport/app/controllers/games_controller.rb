fd115db79ed79aecbc5e27635ab9b63fecd2e797
Alex DeLaPena & Michael Wong <pair+ajdlp.mikey2XU@devbootcamp.com>
2015-08-18 22:59:03 -0700

    Add partial to nes games show

--- passionport/app/controllers/games_controller.rb
+++ passionport/app/controllers/games_controller.rb
@@ -5,8 +5,17 @@ class GamesController < ApplicationController
      render 'show'
   end
 
-  def new
+  def create
+    @games = Game.all
+    unless current_user.games.find_by(title: params[:title])
+      game = Game.find_by(title: params[:title])
+      current_user.games << game if game
+    end
+    redirect_to 'show'
+  end
 
+  def delete
+    OwnedGame.find(params[:owned_game_id]).delete
   end
 
 end
