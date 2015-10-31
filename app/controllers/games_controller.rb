ed7423ab1a6941ec6beaf0e2a25d1eb673261b54
Alex DeLaPena <ajdlp12@gmail.com>
2015-10-30 17:21:20 -0700

    Add clarifying comment to game delete method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,12 +8,12 @@ class GamesController < ApplicationController
     @games = Game.all
     unless current_user.games.find_by(title: params[:title])
       game = Game.find_by(title: params[:title])
-      current_user.games << game if game
+      current_user.games << game if game #associate game with user if game entered in search bar exists
     end
   end
 
   def delete
-    BackloggedGame.find(params[:owned_game_id]).delete
+    BackloggedGame.find(params[:backlogged_game_id]).delete
   end
 
 end
